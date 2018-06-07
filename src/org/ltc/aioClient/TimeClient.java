e65f5299c2c6c174ad0f1eabe98db0e36cf9248e
root <785198525@qq.com>
2018-06-07 10:33:48 +0800

    add-AIO

---
+++ src/org/ltc/aioClient/TimeClient.java
@@ -0,0 +1,15 @@
+package org.ltc.aioClient;
+
+public class TimeClient {
+
+    public static void main(String[] args) {
+        int port = 8080;
+        if (args != null && args.length > 0){
+            port = Integer.valueOf(args[0]);
+        }
+        new Thread(new AsyncTimeClientHandler("127.0.0.1",port),"AIO-AsyncTimeClientHandler-001").start();
+
+    }
+
+
+}
---
+++ src/org/ltc/aioServer/ReadCompletionHandler.java
@@ -0,0 +1,68 @@
+package org.ltc.aioServer;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.nio.channels.CompletionHandler;
+
+public class ReadCompletionHandler implements CompletionHandler<Integer,ByteBuffer> {
+
+    private AsynchronousSocketChannel channel;
+
+    public ReadCompletionHandler(AsynchronousSocketChannel channel) {
+        if (channel != null)
+        this.channel = channel;
+    }
+
+    @Override
+    public void completed(Integer result, ByteBuffer attachment) {
+        attachment.flip();
+        byte[] body = new byte[attachment.remaining()];
+        attachment.get(body);
+
+        try {
+            String req = new String(body,"UTF-8");
+            System.out.println("the timeServer receive order :" + req);
+            String currentTime = "query time order".equalsIgnoreCase(req)? new java.util.Date(System.currentTimeMillis()).toString():"bad order";
+            doWrite(currentTime);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void failed(Throwable exc, ByteBuffer attachment) {
+        try {
+            this.channel.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void doWrite(String currentTime){
+        if (currentTime != null && currentTime.trim().length() > 0){
+            byte[] bytes = currentTime.getBytes();
+            ByteBuffer writeBuffer = ByteBuffer.allocate(1024);
+            writeBuffer.put(bytes);
+            writeBuffer.flip();
+            channel.write(writeBuffer, writeBuffer, new CompletionHandler<Integer, ByteBuffer>() {
+                @Override
+                public void completed(Integer result, ByteBuffer buffer) {
+                    if (buffer.hasRemaining()){
+                        channel.write(buffer,buffer,this);
+                    }
+                }
+
+                @Override
+                public void failed(Throwable exc, ByteBuffer attachment) {
+                    try {
+                        channel.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+    }
+}
